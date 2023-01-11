c650e1b33caf1605061fb61ad60f435d89263af7
jijiseong <jiseong5036@gmail.com>
2023-01-12 01:00:55 +0900

    initial project

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Attendance Check System</Text>
+    </View>
+  );
+};
+
+export default App;
