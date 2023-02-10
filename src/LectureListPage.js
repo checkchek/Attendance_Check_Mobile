1ed2f80f9b890e27406d609a09cf46f9afeb86ab
com5036 <jiseong5036@gmail.com>
2023-02-10 16:25:21 +0900

    Login -> LecturePage

--- src/LectureListPage.js
+++ src/LectureListPage.js
@@ -1,28 +1,15 @@
-import React from 'react';
-import {View, Button} from 'react-native';
+import React, {useState} from 'react';
+import {View, Button, Text} from 'react-native';
 import {launchCamera} from 'react-native-image-picker';
 import {API_URL} from '@env';
 
 export default function LectureListPage({navigation}) {
-  const check = async () => {
-    console.log('click');
-    const result = await launchCamera({
-      includeBase64: true,
-      maxHeight: 1000,
-      maxWidth: 1500,
-    });
+  const [verified, setVerified] = useState();
 
-    const res = await (
-      await fetch(`${API_URL}/api/face`, {
-        method: 'post',
-        body: result.assets[0].base64,
-      })
-    ).json();
-    console.log(res);
-  };
   return (
     <View>
-      <Button title="{현재 진행중인 강의}" onPress={check} />
+      <Button title="{현재 진행중인 강의}" />
+      <Text>{verified ? '인증 완료' : '인증 실패'}</Text>
     </View>
   );
 }
